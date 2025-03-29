91486dfed71eb035c8d5901291e5d325193d381f
Alberto Franco <afranco_dev@outlook.com>
2025-03-29 00:06:25 -0400

    update controller

--- backend/controllers/webhook.js
+++ backend/controllers/webhook.js
@@ -14,8 +14,16 @@ router.post('/', (req, res) => {
   }
 
   switch (event) {
-    case WEBHOOK_VALIDATION:
-      return validateEndpoint();
+    case WEBHOOK_VALIDATION: {
+      const plainToken = req.body.payload.plaintToken;
+
+      const encryptedToken = validateEndpoint(
+        plainToken,
+        process.env.ZOOM_WEBHOOK_SECRET_TOKEN
+      );
+
+      return res.status(200).json({ plainToken, encryptedToken });
+    }
     default:
       console.log(`no match found for event ${event}`);
       return res.status(200).end();
