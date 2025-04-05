e84145e3ddb3b32485527e70dca318b477bd34f9
Arely Martinez <64700714+arelymartinez16@users.noreply.github.com>
2025-04-05 01:00:03 -0400

    Adding event handler for meeting started webhook event (#11)
    * Added event handler for the meeting started event
    
    * Wrote test
    
    * Hopefully fixes prettier errors
    
    * Forgot to replace end time with start time on one line of code

--- backend/controllers/webhook.js
+++ backend/controllers/webhook.js
@@ -3,6 +3,7 @@ import { hashZoomPlainToken } from '../service/webhook.js';
 import {
   WEBHOOK_VALIDATION,
   WEBHOOK_MEETING_ENDED,
+  WEBHOOK_MEETING_STARTED,
 } from '../service/events.js';
 
 const router = Router();
@@ -33,6 +34,13 @@ router.post('/', (req, res) => {
 
       console.log(`Meeting ${meetingName} ended at ${timeEnded}`);
       return res.status(200).json({ message: 'Meeting has ended.' });
+    }
+    case WEBHOOK_MEETING_STARTED: {
+      const meetingName = req.body.payload.object.topic;
+      const timeStarted = req.body.payload.object.start_time;
+
+      console.log(`Meeting ${meetingName} started at ${timeStarted}`);
+      return res.status(201).json({ message: 'Meeting has started.' });
     }
     default:
       console.log(`no match found for event ${event}`);
--- backend/service/events.js
+++ backend/service/events.js
@@ -1,2 +1,3 @@
 export const WEBHOOK_VALIDATION = 'endpoint.url_validation';
 export const WEBHOOK_MEETING_ENDED = 'meeting.ended';
+export const WEBHOOK_MEETING_STARTED = 'meeting.started';
