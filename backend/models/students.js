60f209fdcb19e1fae57efd720208c16924407e74
arelymartinez16 <arelymartinez741@gmail.com>
2025-04-11 19:07:59 -0400

    Updated models/students.js to follow es6 standards

--- backend/models/students.js
+++ backend/models/students.js
@@ -1,25 +1,19 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Students extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Students.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    student_id: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Students',
-  });
-  return Students;
-};
+import { Model } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+    class Students extends Model {}
+    Students.init({
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      student_id: DataTypes.STRING
+    }, {
+      sequelize,
+      modelName: 'Students',
+    });
+
+    Students.associate = (/* models */) => {
+      // associations can be defined here
+    };
+
+    return Students;
+}
