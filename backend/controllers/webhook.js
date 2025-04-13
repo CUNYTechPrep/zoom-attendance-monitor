8b1e8af8fca82441c8764b57c1a06aecddaf21e5
Carlo Ace Sagad <carloace166@gmail.com>
2025-04-13 00:00:02 -0400

    Fix webhook issue

--- backend/controllers/webhook.js
+++ backend/controllers/webhook.js
@@ -4,6 +4,7 @@ import {
   WEBHOOK_VALIDATION,
   WEBHOOK_MEETING_ENDED,
   WEBHOOK_MEETING_STARTED,
+  WEBHOOK_RECORDING_COMPLETED,
 } from '../service/events.js';
 
 const router = Router();
@@ -28,6 +29,7 @@ router.post('/', (req, res) => {
 
       return res.status(200).json({ plainToken, encryptedToken });
     }
+
     case WEBHOOK_MEETING_ENDED: {
       const meetingName = req.body.payload.object.topic;
       const timeEnded = req.body.payload.object.end_time;
@@ -35,6 +37,7 @@ router.post('/', (req, res) => {
       console.log(`Meeting ${meetingName} ended at ${timeEnded}`);
       return res.status(200).json({ message: 'Meeting has ended.' });
     }
+
     case WEBHOOK_MEETING_STARTED: {
       const meetingName = req.body.payload.object.topic;
       const timeStarted = req.body.payload.object.start_time;
@@ -42,6 +45,13 @@ router.post('/', (req, res) => {
       console.log(`Meeting ${meetingName} started at ${timeStarted}`);
       return res.status(201).json({ message: 'Meeting has started.' });
     }
+
+    case WEBHOOK_RECORDING_COMPLETED: {
+      const recordingName = req.body.payload.object.topic || 'Unnamed Meeting';
+      console.log(`Recording "${recordingName}" has completed.`); // Log the completion message
+      return res.status(200).json({ message: 'Recording completed.' });
+    }
+
     default:
       console.log(`no match found for event ${event}`);
       return res.status(200).end();
