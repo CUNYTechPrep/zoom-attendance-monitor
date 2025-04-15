03f464cf461a6ce80e83258c3c69c94e53f3ac18
Jude <86798171+jpntc@users.noreply.github.com>
2025-04-15 19:19:35 -0400

    fixed prev pull request issues - user joined meeting event hooks feature

--- backend/tests/participant-joined.test.js
+++ backend/tests/participant-joined.test.js
@@ -1,59 +1,57 @@
 import request from 'supertest';
 import app from '../app';
-import { WEBHOOK_PARTICIPANT_JOINED} from '../service/events.js';
-import { WEBHOOK_PARTICIPANT_JOINED_BH} from '../service/events.js';
-
+import { WEBHOOK_PARTICIPANT_JOINED } from '../service/events.js';
+import { WEBHOOK_PARTICIPANT_JOINED_BH } from '../service/events.js';
 
 const participantJoined = {
-    "account_id": "AAAAAABBBB",
-    "object": {
-      "participant": {
-        "user_id": "1234567890",
-        "user_name": "Jill Chill",
-        "email": "jchill@example.com",
-        "phone_number": "8615250064084"
-      }
-    }
+  object: {
+    participant: {
+      participant_uuid: '1234567890',
+      user_name: 'Jill Chill',
+      email: 'jchill@example.com',
+      phone_number: '8615250064084',
+      join_time: '2025-04-15',
+    },
+  },
 };
-const participantJoinedBeforeHost =   {
-    "account_id": "AAAAAABBBB",
-    "object": {
-      "participant": {
-        "user_id": "1234567890",
-        "user_name": "Jill Chill",
-        "email": "jchill@example.com",
-        "phone_number": "8615250064084"
-      }
-  }};
+const participantJoinedBeforeHost = {
+  object: {
+    participant: {
+      participant_uuid: '1234567890',
+      user_name: 'Jill Chill',
+      email: 'jchill@example.com',
+      phone_number: '8615250064084',
+      join_time: '2025-04-15',
+    },
+  },
+};
 
+async function handleRequest(userData, eventToTrigger, expectedReturnValue) {
+  const response = await request(app)
+    .post('/api/webhook')
+    .send({
+      event: eventToTrigger,
+      payload: {
+        ...userData,
+      },
+    });
 
-async function handleRequest(userData, eventToTrigger, expectedReturnValue){
-
-    const response = await request(app)
-      .post('/api/webhook')
-      .send({
-        event: eventToTrigger,
-        payload: {
-          ...userData,
-        },
-      })
-
-      const parsedRes = await JSON.parse(response.text);
-      expect(response.statusCode).toBe(200);
-      expect(parsedRes).toBe(expectedReturnValue);
-
+  expect(response.statusCode).toBe(200);
+  const parsedRes = JSON.parse(response.text)
+  expect(parsedRes).toEqual(expectedReturnValue);
 }
 
-
-describe("participant joined", () =>{
-    test('trigger and compare backend response', ()=>handleRequest(participantJoined, WEBHOOK_PARTICIPANT_JOINED,
-           "{\"account_id\":\"AAAAAABBBB\",\"user_name\":\"Jill Chill\",\"phone_number\":\"8615250064084\"}"
-    ))
-      test(
-        'trigger and compare backend response',
-        ()=> handleRequest(participantJoinedBeforeHost, WEBHOOK_PARTICIPANT_JOINED_BH, 
-          "{\"account_id\":\"AAAAAABBBB\",\"user_name\":\"Jill Chill\"}")
-      );
-
-
+describe('participant joined', () => {
+  test('trigger and compare backend response', () =>
+    handleRequest(participantJoined, WEBHOOK_PARTICIPANT_JOINED, {
+      participant_id: '1234567890',
+      participant_name: 'Jill Chill',
+      join_time: '2025-04-15',
+    }));
+  test('trigger and compare backend response', () =>
+    handleRequest(participantJoinedBeforeHost, WEBHOOK_PARTICIPANT_JOINED_BH, {
+      participant_id: '1234567890',
+      participant_name: 'Jill Chill',
+      join_time: '2025-04-15',
+    }));
 });
