287f951f9dc987dc2518375b29d8f5a2aed4ef9e
Jude <86798171+jpntc@users.noreply.github.com>
2025-04-25 00:09:45 -0400

    eslint checks and fix

--- backend/models/Meeting.js
+++ backend/models/Meeting.js
@@ -1,26 +1,50 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class create - meetings extends Model {
+import { Model } from 'sequelize';
+
+export default (sequelize, DataTypes) =>{
+  class Meeting extends Model {}
+
+    Meeting.init(
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.STRING,
+        },
+        meeting_name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        start: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        end: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: DataTypes.DATE,
+        },
+      },
+      {
+        sequelize,
+        modelName: 'Meeting',
+      }
+    );
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-    }
-  }
-  create - meetings.init({
-    meeting_name: DataTypes.STRING,
-    id: DataTypes.INTEGER,
-    start: DataTypes.STRING,
-    end: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'create-meetings',
-  });
-  return create - meetings;
-};
+  //  Meeting.associate(models) {
+  //     // define association here
+  //   }
+
+  return Meeting;
+}
